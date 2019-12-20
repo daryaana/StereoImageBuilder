f07c8088080d1aafa4b2fca0999cd8dce70bdddf
Darya Smirnova <sda_19991999@mail.ru>
2019-12-21 00:03:32 +0700

    create generator StereoPic

--- src/main/java/StereoImageCreator.java
+++ src/main/java/StereoImageCreator.java
@@ -1,2 +1,35 @@
+import java.awt.image.BufferedImage;
+
 public class StereoImageCreator {
+    public BufferedImage generateStereoPicture(BufferedImage bitmapMask, BufferedImage stereoImg)
+    {
+        // Переводим маску в массив сдвигов
+        int w = bitmapMask.getWidth();
+        int h = bitmapMask.getHeight();
+        int[][] mask = new int[w][];
+        for (int x = 0; x < w; x++)
+        {
+            mask[x] = new int[h];
+            for (int y = 0; y < h; y++) {
+                int rgb = bitmapMask.getRGB(x,y);
+                int red = (rgb >> 16) & 0xFF;
+                mask[x][y] = red / 32;
+            }
+        }
+
+        // Cоздаем фон
+        int s = 100;
+
+        // Сдвигаем каждый пиксел
+        for (int y = 0; y < h; y++)
+            for (int x = 0; x < w; x++)
+                if (mask[x][y] > 0)
+                {
+                    int pixel = stereoImg.getRGB(x + mask[x][y], y);
+                    for (int i = x + s; i < w + s; i += s)
+                        stereoImg.setRGB(i, y, pixel);
+                }
+
+        return stereoImg;
+    }
 }
